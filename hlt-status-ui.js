3a415101c60c8f3faf0ef426a8bf9b9aff01f8f3
Heiko Engel <hengel@cern.ch>
2016-08-03 11:15:18 +0200

    status-ui: formatting

--- hlt-status-ui.js
+++ hlt-status-ui.js
@@ -6,7 +6,7 @@
 
 // auto-update every 2s
 var inter = setInterval(function() {
-  drawgraphs();
+    drawgraphs();
 }, 2000);
 
 
@@ -25,28 +25,22 @@ function onClickTab(tab) {
 
 /** handle clicks on Freeze/Resume button **/
 function toggleInterval() {
-  var ctrlbtn = d3.select("#ctrlButton");
-  var state = 1;
-  var b = ctrlbtn.select(".btn-danger");
-  if (ctrlbtn.select(".btn-danger").empty()) {
-    b = ctrlbtn.select(".btn-success");
-    state = 0;
-  }
+    var ctrlbtn = d3.select("#ctrlButton");
+    var state = 1;
+    var b = ctrlbtn.select(".btn-danger");
+    if (ctrlbtn.select(".btn-danger").empty()) {
+	b = ctrlbtn.select(".btn-success");
+	state = 0;
+    }
 
-  if (state == 0) {
-    console.log("Resuming...");
-    inter = setInterval( function() { drawgraphs(); }, 2000);
-    b.classed("btn-success", false);
-    b.classed("btn btn-danger pull-right", true);
-    b.text("Freeze");
-    b.on('click', toggleInterval);
-    drawgraphs();
-  } else {
-    console.log("Stopping...");
-    clearInterval(inter);
-    b.classed("btn btn-success pull-right", true);
-    b.classed("btn-danger", false);
-    b.text("Resume");
-    b.on('click', toggleInterval);
-  }
+    if (state == 0) {
+	inter = setInterval( function() { drawgraphs(); }, 2000);
+	b.classed("btn-success", false).classed("btn btn-danger pull-right", true)
+	    .text("Freeze").on('click', toggleInterval);
+	drawgraphs();
+    } else {
+	clearInterval(inter);
+	b.classed("btn btn-success pull-right", true).classed("btn-danger", false)
+	    .text("Resume").on('click', toggleInterval);
+    }
 }
